f9bc089b831b45d0f1d782084bac66fc98b02665
heopill <heopill@naver.com>
2025-03-11 20:38:40 +0900

    feat: switch문으로 변경
    기존 4개의 func 코드에서 1개의 switch문으로 변경
    0으로 나누었을 때의 예외처리 상황 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,33 +1,40 @@
 class Calculator {
-    func add (num1: Int, num2: Int) -> Int {
-        return num1 + num2
-    }
-    
-    func sub (num1: Int, num2: Int) -> Int {
-        return num1 - num2
-    }
-    
-    func mul (num1: Int, num2: Int) -> Int {
-        return num1 * num2
-    }
-    
-    func div(num1: Float, num2: Float) -> Float? {
-        if num2 == 0 {
-            print("0으로 나눌 수 없습니다.")
-            return nil
+    func calculate (op: String, num1: Int, num2: Int) -> String {
+        
+        switch op {
+        case "+":
+            return "\(num1+num2)"
+        case "-":
+            return "\(num1-num2)"
+        case "*":
+            return "\(num1*num2)"
+        case "/":
+            if num1 == 0 || num2 == 0 {
+                return "0으로 나눌 수 없습니다."
+            }
+            return "\(num1/num2)"
+        case "%":
+            return "\(num1%num2)"
+        default:
+            return "에러 발생"
         }
-        return num1 / num2
-    }
-    
-    func mod (num1: Int, num2: Int) -> Int {
-        return num1 % num2
     }
 }
 
 let calculator = Calculator()
 
-print(calculator.add(num1: 20, num2: 10))
-print(calculator.sub(num1: 20, num2: 10))
-print(calculator.mul(num1: 20, num2: 10))
-print(calculator.div(num1: 20, num2: 0) ?? "")
-print(calculator.mod(num1: 20, num2: 7))
+let addResult = calculator.calculate(op: "+", num1: 10, num2: 5)
+print(addResult)
+
+let subResult = calculator.calculate(op: "-", num1: 10, num2: 5)
+print(subResult)
+
+let mulResult = calculator.calculate(op: "*", num1: 10, num2: 5)
+print(mulResult)
+
+let divResult = calculator.calculate(op: "/", num1: 10, num2: 0)
+print(divResult)
+
+let modResult = calculator.calculate(op: "%", num1: 10, num2: 3)
+print(modResult)
+
