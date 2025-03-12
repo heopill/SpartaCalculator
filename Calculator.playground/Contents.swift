30f57f43b24801541a64e9ed577b0ea9ca8cc30c
heopill <heopill@naver.com>
2025-03-12 15:35:30 +0900

    feat: Type Casting
    calculate의 num1, num2의 데이터 타입을 Double로 변경
    나누기와 나머지 연산에서 num1과 num2의 연산 후 데이터 타입 변경

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,20 +1,25 @@
 class Calculator {
-    func calculate (op: String, num1: Int, num2: Int) -> String {
+    func calculate (op: String, num1: Double, num2: Double) -> String {
         
         switch op {
         case "+":
-            return "\(num1+num2)"
+            var result = Int(num1 + num2)
+            return "\(result)"
         case "-":
-            return "\(num1-num2)"
+            var result = Int(num1 - num2)
+            return "\(result)"
         case "*":
-            return "\(num1*num2)"
+            var result = Int(num1 * num2)
+            return "\(result)"
         case "/":
             if num1 == 0 || num2 == 0 {
                 return "0으로 나눌 수 없습니다."
             }
-            return "\(num1/num2)"
+            var result = Float(num1/num2)
+            return "\(result)"
         case "%":
-            return "\(num1%num2)"
+                var result = Int(num1) % Int(num2)
+                return "\(result)"
         default:
             return "에러 발생"
         }
@@ -32,9 +37,12 @@ print(subResult)
 let mulResult = calculator.calculate(op: "*", num1: 10, num2: 5)
 print(mulResult)
 
-let divResult = calculator.calculate(op: "/", num1: 10, num2: 0)
+let divResult = calculator.calculate(op: "/", num1: 10, num2: 3)
 print(divResult)
+
+// 0으로 나누었을 때 (예외처리)
+let divResult2 = calculator.calculate(op: "/", num1: 10, num2: 0)
+print(divResult2)
 
 let modResult = calculator.calculate(op: "%", num1: 10, num2: 3)
 print(modResult)
-
