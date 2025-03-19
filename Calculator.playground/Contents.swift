cd5ffe5a2f232d83db77756c19a5a22a109fc0c8
heopill <heopill@naver.com>
2025-03-19 15:44:15 +0900

    refactor: code change
    코드 수정

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,73 +1,76 @@
+class AddOperation {
+    func add(_ number1: Int, _ number2: Int) -> Double {
+        return Double(number1 + number2)
+    }
+}
+
+class SubstractOperation {
+    func minus(_ number1: Int, _ number2: Int) -> Double {
+        return Double(number1 - number2)
+    }
+}
+
+class MultiplyOperation {
+    func multiply(_ number1: Int, _ number2: Int) -> Double {
+        return Double(number1 * number2)
+    }
+}
+
+class DivideOperation {
+    func devide(_ number1: Int, _ number2: Int) -> Double {
+        guard number2 != 0 else {
+            return 0.0
+        }
+        return Double(number1) / Double(number2)
+    }
+}
+
+class RemainderOperation {
+    func mod(_ number1: Int, _ number2: Int) -> Double {
+        guard number2 != 0 else {
+            return 0
+        }
+        return Double (number1 % number2)
+    }
+}
+
 class Calculator {
-    var num1: Double
-    var num2: Double
+    let addOperation = AddOperation()
+    let substractOperation = SubstractOperation()
+    let multiplyOperation = MultiplyOperation()
+    let divideOperation = DivideOperation()
+    let remainderOperation = RemainderOperation()
     
-    init(num1: Double, num2: Double) {
-        self.num1 = num1
-        self.num2 = num2
-    }
-    
-    func result() -> Double? {
-        return nil
-    }
-}
-
-class AddOperation : Calculator {
-    override func result() -> Double? {
-        let result = num1 + num2
-        return result
-    }
-}
-
-class SubstractOperation : Calculator {
-    override func result() -> Double? {
-        let result = num1 - num2
-        return result
-    }
-}
-class MultiplyOperation : Calculator {
-    override func result() -> Double? {
-        let result = num1 * num2
-        return result
-    }
-}
-class DivideOperation : Calculator {
-    override func result() -> Double? {
-        if num1 == 0 || num2 == 0{
-            return nil
+    func operate(_ operator: OperatorType, _ number1: Int, _ number2: Int) -> Double {
+        switch `operator` {
+        case .add:
+            return addOperation.add(number1, number2)
+        case .minus:
+            return substractOperation.minus(number1, number2)
+        case .multiply:
+            return multiplyOperation.multiply(number1, number2)
+        case .divide:
+            return divideOperation.devide(number1, number2)
+        case .mod:
+            return remainderOperation.mod(number1, number2)
         }
-        let result = num1 / num2
-        return result
+        
     }
 }
 
-class RemainderOperation : Calculator {
-    func resultMod() -> Int? {
-        if num1 == 0 || num2 == 0{
-            return nil
-        }
-        let result = Int(num1) % Int(num2)
-        return result
-    }
+enum OperatorType {
+    case add
+    case minus
+    case multiply
+    case divide
+    case mod
 }
 
-let add = AddOperation(num1: 2.3, num2: 3.2)
-print(add.result()!)
+let calculator = Calculator() // 인스턴스 생성하여 변수에 할당
 
-let sub = SubstractOperation(num1: 4.5, num2: 3.0)
-print(sub.result()!)
+print(calculator.operate(.add, 1, 2))
+print(calculator.operate(.minus, 1, 2))
+print(calculator.operate(.multiply, 1, 2))
+print(calculator.operate(.divide, 1, 0))
+print(calculator.operate(.mod, 4, 3))
 
-let mul = MultiplyOperation(num1: 10, num2: 4)
-print(mul.result()!)
-
-let div = DivideOperation(num1: 30, num2: 5)
-print(div.result() ?? "0으로 나눌 수 없습니다.")
-
-let div0 = DivideOperation(num1: 30, num2: 0) // 나누는 수가 0일 때 예외 처리 예시
-print(div0.result() ?? "0으로 나눌 수 없습니다.")
-
-let mod = RemainderOperation(num1: 10, num2: 3)
-print(mod.resultMod() ?? "0으로 나눌 수 없습니다.")
-
-let mod0 = RemainderOperation(num1: 10, num2: 0) // 나누는 수가 0일 때 예외 처리 예시
-print(mod0.resultMod() ?? "0으로 나눌 수 없습니다.")
